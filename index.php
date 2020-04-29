faf53f00e095ddbf01692cc0f4f93f2459ce4825
Snaige <ssnaige@hotmail.com>
2020-04-29 23:09:12 +0300

    MySQL tables HTML
    workbench sukurtos lenteles atvaizdavimas html

--- index.php
+++ index.php
@@ -1,17 +1,98 @@
-
-
-<h3>DAUGINAME BET KOKIUS SKAICIUS</h3> 
-<form action = "" method= "post">
-    <input type= "text" name="n" value= "" placeholder="irasykite skaiciu"><br>
-    <input type= "text" name="n2" value= "" placeholder="irasykite skaiciu"><br>
-    <input type= "submit" name="act" value= "dauginti" >
-</form>
-<?php
-if(isset($_POST['act']))
-{
-    $n = $_POST['n'];
-    $n2 = $_POST['n2'];
-    $add = $n*$n2;
-    echo "Atsakymas : " . $add;
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+  border: 1px solid grey  
 }
-?>
+
+th, td {
+  text-align: left;
+  padding: 8px;
+  height: 20px;
+  border-left: 1px solid grey;
+  
+}
+   
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4CAF50;
+  color: white;
+  
+}
+.button{
+color:  black;
+width: 235px;
+
+}
+</style>
+</head>
+<body>
+<table border=1>
+<tr>
+<td bgcolor=silver class='medium'>ID</td>
+<td bgcolor=silver class='medium'>Vardas</td>
+<td bgcolor=silver class='medium'>Projektas</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>1</td>
+<td class='normal' valign='top'>Linas</td>
+<td class='normal' valign='top'>Debesu Ganimas</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>2</td>
+<td class='normal' valign='top'>Linas</td>
+<td class='normal' valign='top'>Svilpynes</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>3</td>
+<td class='normal' valign='top'>Audrus</td>
+<td class='normal' valign='top'>Debesu Ganymas</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>4</td>
+<td class='normal' valign='top'>Stasys</td>
+<td class='normal' valign='top'>Skuduciai</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>5</td>
+<td class='normal' valign='top'>Rokas</td>
+<td class='normal' valign='top'>Svilpynes</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>6</td>
+<td class='normal' valign='top'>Saulius</td>
+<td class='normal' valign='top'>Skuduciai</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>7</td>
+<td class='normal' valign='top'>Marius</td>
+<td class='normal' valign='top'>Gargaliavimas</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>8</td>
+<td class='normal' valign='top'>Vytas</td>
+<td class='normal' valign='top'>Skuduciai</td>
+</tr>
+
+<tr>
+<td class='normal' valign='top'>9</td>
+<td class='normal' valign='top'>Saulius</td>
+<td class='normal' valign='top'>Svilpynes</td>
+</tr>
+</table>
+</body></html>
+
+<br>
+
